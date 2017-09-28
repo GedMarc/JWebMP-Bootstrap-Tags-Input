a20a79c2b7e1ed5770f941f82039bdecfd2e3f02
Marc Magon <ged_marc@hotmail.com>
2017-09-28 03:56:04 +0200

    JUnit 5 updates, Pretty Checkboxes implementation and a lot more
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/bootstraptagsinput/BootstrapTagsInputFeatureTest.java
+++ test/za/co/mmagon/jwebswing/plugins/bootstraptagsinput/BootstrapTagsInputFeatureTest.java
@@ -5,7 +5,7 @@
  */
 package za.co.mmagon.jwebswing.plugins.bootstraptagsinput;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import za.co.mmagon.BaseTestClass;
 
 /**
--- test/za/co/mmagon/jwebswing/plugins/bootstraptagsinput/BootstrapTagsInputOptionsTest.java
+++ test/za/co/mmagon/jwebswing/plugins/bootstraptagsinput/BootstrapTagsInputOptionsTest.java
@@ -1,6 +1,6 @@
 package za.co.mmagon.jwebswing.plugins.bootstraptagsinput;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Marc Magon
