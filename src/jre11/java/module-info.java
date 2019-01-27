ea6eabea5eb9e0f0cc5e6910c41ce4d04a6bb162
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:52 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.bootstraptagsinput.BootstrapTagsInputPageConfigurator;
-import com.jwebmp.plugins.bootstraptagsinput.implementations.BootstrapTagsInputExclusionsModule;
-
 module com.jwebmp.plugins.bootstraptagsinput {
 	exports com.jwebmp.plugins.bootstraptagsinput;
 
@@ -15,10 +9,10 @@ module com.jwebmp.plugins.bootstraptagsinput {
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with BootstrapTagsInputPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.bootstraptagsinput.BootstrapTagsInputPageConfigurator;
 
-	provides IGuiceScanJarExclusions with BootstrapTagsInputExclusionsModule;
-	provides IGuiceScanModuleExclusions with BootstrapTagsInputExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bootstraptagsinput.implementations.BootstrapTagsInputExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bootstraptagsinput.implementations.BootstrapTagsInputExclusionsModule;
 
 	opens com.jwebmp.plugins.bootstraptagsinput to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
